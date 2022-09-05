0c83a7f54fa4be3e86fa30d095b5c1aa3d7c8b63
William Phan <hoangviet191@gmail.com>
2022-09-05 20:22:14 +0700

    algorithm - sorted - pair sum

---
+++ udacity/basic_algorithms/14_pair-sum.py
@@ -0,0 +1,64 @@
+# Given an input array and a target value (integer),
+# find two values in the array whose sum is equal to the target value.
+# Solve the problem without using extra space.
+# You can assume the array has unique values and will never have more than one solution.
+
+def pair_sum(arr, target):
+    """
+    :param: arr - input array
+    :param: target - target value
+    TODO: complete this method to find two numbers such that their sum is equal to the target
+    Return the two numbers in the form of a sorted list
+    """
+    # sort the array
+    arr.sort()
+
+    # initialize two pointer - one from start of the array and other from the end
+    front_index = 0
+    back_index = len(arr) - 1
+
+    # shift the pointer
+    while front_index < back_index:
+        front = arr[front_index]
+        back = arr[back_index]
+
+        if front + back == target:
+            return [front, back]
+        elif front + back < target:
+            front_index += 1
+        else:
+            back_index -= 1
+
+
+    return [None, None]
+
+
+def test_function(test_case):
+    input_list = test_case[0]
+    target =test_case[1]
+    solution = test_case[2]
+    output = pair_sum(input_list, target)
+    if output == solution:
+        print("Pass")
+    else:
+        print("False")
+
+
+if __name__ == '__main__':
+    input_list = [2, 7, 11, 15]
+    target = 9
+    solution = [2, 7]
+    test_case = [input_list, target, solution]
+    test_function(test_case)
+
+    input_list = [0, 8, 5, 7, 9]
+    target = 9
+    solution = [0, 9]
+    test_case = [input_list, target, solution]
+    test_function(test_case)
+
+    input_list = [110, 9, 89]
+    target = 9
+    solution = [None, None]
+    test_case = [input_list, target, solution]
+    test_function(test_case)
